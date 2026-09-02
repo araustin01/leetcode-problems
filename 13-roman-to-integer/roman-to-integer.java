090731c12416cd68910816927b0528cbc04dfc53
Alex <72994707+araustin01@users.noreply.github.com>
2026-09-02 13:02:24 -0500

    Time: 3 ms (76.96%) | Memory: 46.5 MB (62.07%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,25 +1,24 @@
 class Solution {
 
-    private static final Map<String, Integer> SYMBOL_MAP = new HashMap<String, Integer> () {{
-        put("I", 1);
-        put("V", 5);
-        put("X", 10);
-        put("L", 50);
-        put("C", 100);
-        put("D", 500);
-        put("M", 1000);
+    private static final Map<Character, Integer> SYMBOL_MAP = new HashMap<Character, Integer> () {{
+        put('I', 1);
+        put('V', 5);
+        put('X', 10);
+        put('L', 50);
+        put('C', 100);
+        put('D', 500);
+        put('M', 1000);
     }};
 
     public int romanToInt(String s) {
-        String[] chars = s.split("");
         int total = 0;
 
-        for (int i=0, j=1; i < chars.length; i++, j++) {
-            int value = SYMBOL_MAP.get(chars[i]); // current roman symbol value
+        for (int i=0, j=1; i < s.length(); i++, j++) {
+            int value = SYMBOL_MAP.get(s.charAt(i)); // current roman symbol value
             int sflag = 1; // sign flag
 
-            if (j < chars.length) {
-                int next_value = SYMBOL_MAP.get(chars[j]); // next roman symbol value
+            if (j < s.length()) {
+                int next_value = SYMBOL_MAP.get(s.charAt(j)); // next roman symbol value
                 if (next_value > value) {
                     sflag = -1;
                 }
